1fd17821120a5983bf2a41d47ea47381f5f700f0
zFlayn <pablo.capistran123@gmail.com>
2022-05-25 12:13:29 -0500

    Practica 3
    Practica 3

---
+++ Practica3-26feb/src/app/tab1/tab1.page.ts
@@ -0,0 +1,100 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  constructor() {}
+
+  alumnos = [
+    {
+      "nombre": "Pablo",
+      "apellido": "Capistran",
+      "matricula": "1971973"
+    },
+    {
+      "nombre": "Cristhian",
+      "apellido": "Juarez",
+      "matricula": "1905089"
+    },
+    {
+      "nombre": "Hugo",
+      "apellido": "Meza",
+      "matricula": "1909299"
+    },
+    {
+      "nombre": "Diego",
+      "apellido": "Martinez",
+      "matricula": "1234879"
+    },
+    {
+      "nombre": "Angel",
+      "apellido": "Medina",
+      "matricula": "45443531"
+    },
+    {
+      "nombre": "Ricardo",
+      "apellido": "Martinez",
+      "matricula": "32512314"
+    },
+    {
+      "nombre": "Mario",
+      "apellido": "Salazar",
+      "matricula": "432411423"
+    },
+    {
+      "nombre": "Ian",
+      "apellido": "Ramirez",
+      "matricula": "123124124"
+    },
+    {
+      "nombre": "Emiliano",
+      "apellido": "Eguias",
+      "matricula": "421412421"
+    },
+    {
+      "nombre": "Sergio",
+      "apellido": "Ramirez",
+      "matricula": "12412415"
+    },
+    {
+      "nombre": "Willy",
+      "apellido": "Diaz",
+      "matricula": "124124515"
+    },
+    {
+      "nombre": "Jose",
+      "apellido": "Capistran",
+      "matricula": "214124121"
+    },
+    {
+      "nombre": "David",
+      "apellido": "Rocha",
+      "matricula": "12412456"
+    },
+    {
+      "nombre": "Leonardo",
+      "apellido": "Rodriguez",
+      "matricula": "12412415"
+    },
+    {
+      "nombre": "Luis",
+      "apellido": "Mavin",
+      "matricula": "1241256"
+    },
+    {
+      "nombre": "Ana",
+      "apellido": "Luna",
+      "matricula": "12414214"
+    },
+    {
+      "nombre": "Alma",
+      "apellido": "Luna",
+      "matricula": "124124125"
+    }
+  ];
+
+}
